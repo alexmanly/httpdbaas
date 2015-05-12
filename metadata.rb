0e357da52d35a3cccf39d2de6b75a80b77b090e2
Bakh Inamov <b@chef.io>
2015-05-12 09:13:22 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'httpdbaas'
+maintainer 'The Aristocrats'
+maintainer_email 'b@chef.io'
+license 'all_rights'
+description 'Installs/Configures httpdbaas'
+long_description 'Installs/Configures httpdbaas'
+version '0.1.0'
+
+depends 'apt'
